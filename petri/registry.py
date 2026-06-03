2fdc28f6f3a92d0807982a6e26241c4267172319
Arun Raghunath <arun2aruns@gmail.com>
2026-06-03 21:31:54 +0100

    feat: in-memory registry with SandboxNotFound exception

---
+++ petri/registry.py
@@ -0,0 +1,22 @@
+from petri.sandbox import Sandbox
+
+class SandboxNotFound(Exception):
+    pass
+    
+class Registry:
+    def __init__(self) -> None:
+        self._sandboxes: dict[str, Sandbox] = {}
+
+    def add(self, sandbox: Sandbox) -> None:
+        self._sandboxes[sandbox.id] = sandbox
+
+    def get(self, sandbox_id: str) -> Sandbox:
+        if sandbox_id not in self._sandboxes:
+            raise SandboxNotFound(sandbox_id)
+
+        return self._sandboxes[sandbox_id]
+
+    def remove(self, sandbox_id: str) -> None:
+        if sandbox_id not in self._sandboxes:
+            raise SandboxNotFound(sandbox_id)
+        del self._sandboxes[sandbox_id]
